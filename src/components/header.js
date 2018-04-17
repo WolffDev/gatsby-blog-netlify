78b6fe18ffbad435a6565790d545313b2e0f8996
David Wolff <info@davidwolff.dk>
2018-04-17 11:02:01 +0200

    added styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,45 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
+import logo from '../images/gatsby.svg';
+
+const HeaderWrapper = styled.div`
+  background: #524763;
+  margin-bottom: 1.45rem;
+  a {
+    color: #fff;
+  }
+  img {
+    height: 100px;
+  }
+`;
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
+
+const Header = () => (
+  <HeaderWrapper>
+    <HeaderContainer>
+      <h1>
+        <Link to="/">
+          <img src={logo} alt="Gatsbyjs logo" />
         </Link>
       </h1>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 export default Header
