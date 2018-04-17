7a8355d6380d035592ebab174d039a5e7a27ba99
David Wolff <info@davidwolff.dk>
2018-04-17 10:43:25 +0200

    first init

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default class AboutPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to ABOUT page</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In voluptas aspernatur molestias nisi placeat earum cum, doloremque quod eligendi error at consequatur ex repellendus hic reprehenderit excepturi, natus nam sint. Aliquid voluptate magnam animi exercitationem nisi sunt odit nihil ipsum.
+        </p>
+      </div>
+    )
+  }
+};
